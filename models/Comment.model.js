809bef7c9c25cb837807d357f40669c72a479203
Mariana Franco <marianamedeirosfranco@gmail.com>
2023-03-10 17:45:51 +0000

    routes&models update

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 const commentSchema = new Schema({
   description: { type: String },
   user: { type: Schema.Types.ObjectId, ref: "User" },
+  taskId: {type: Schema.Types.ObjectId, ref: "Task"}
 },
 {
     timestamps: true,
--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const mongoose = require('mongoose')
 
 //import the models
 const Task = require('../models/Task.model');
@@ -7,15 +8,15 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
 //Create
 
-router.post('/comments/:id', isAuthenticated, async (req, res, next) => {
+router.post('/comments/:taskId', isAuthenticated, async (req, res, next) => {
   const { description } = req.body;
-  const {id} = req.params
+  const {taskId} = req.params
   // const _id = req.payload._id;
 
   try {
-    const comments = await Comment.create({ description });
+    const comments = await Comment.create({ description, taskId });
 
-    await Task.findByIdAndUpdate(id, {
+    await Task.findByIdAndUpdate(taskId, {
       $push: {
         comments: comments._id,
       },
@@ -27,19 +28,23 @@ router.post('/comments/:id', isAuthenticated, async (req, res, next) => {
   }
 });
 
-router.delete('/comments/:id', async (req, res, next) => {
-  const { id } = req.params;
+router.delete('/comments/:commentId', async (req, res, next) => {
+  const { commentId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
+  if (!mongoose.Types.ObjectId.isValid(commentId)) {
     res.json('The provided comment id is not valid');
   }
 
   try {
     //remove a specific comment from a task
-    const task = await Task.findById(id);
-    await Comment.findByIdAndDelete({ _id: task.comments });
+    const findComment = await Comment.findByIdAndDelete(commentId);
+    const task = await Task.findByIdAndUpdate(findComment.taskId, {
+      $pull: {
+        comments: commentId,
+      },});
+    
 
-    res.json({ message: `Comment with the id ${id} deleted successfully` });
+    res.json({ message: `Comment deleted successfully` });
   } catch (error) {
     res.json(error);
   }
