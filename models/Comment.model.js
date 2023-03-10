c61dce92119549071c896ddfd977066d8227feb7
Mariana Franco <marianamedeirosfranco@gmail.com>
2023-03-10 13:33:05 +0000

    updated v

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require("mongoose");
 
-const commentSchema = new mongoose.Schema({
+const commentSchema = new Schema({
   description: { type: String },
   user: { type: Schema.Types.ObjectId, ref: "User" },
 });
