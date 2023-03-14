e24afec62edd0d847461d2ecb3e320836c08211c
Mariana Franco <marianamedeirosfranco@gmail.com>
2023-03-14 13:19:54 +0000

    updates

--- models/Task.model.js
+++ models/Task.model.js
@@ -1,20 +1,20 @@
 const { Schema, model } = require("mongoose");
 
-const taskSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  status: { type: String, enum: ["Pending", "In Progress", "Completed"] },
-  importance: {
-    type: String,
-    enum: ["High Priority", "Important", "Normal"],
-    default: "Normal",
+const taskSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: String, enum: ["Pending", "In Progress", "Completed"] },
+    importance: {
+      type: String,
+      enum: ["High Priority", "Important", "Normal"],
+      default: "Normal",
+    },
+    date: { Date },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   },
-  date: {Date},
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-
-},
-{
+  {
     timestamps: true,
   }
 );
