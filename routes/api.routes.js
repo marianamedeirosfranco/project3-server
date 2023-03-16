56e1884076fc9ac22001ad8669f5f05a082b4009
Mariana Franco <marianamedeirosfranco@gmail.com>
2023-03-16 12:04:33 +0000

    env

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,28 +1,27 @@
 const router = require("express").Router();
 const { google } = require("googleapis");
 
-const User = require('../models/User.model')
-const Task = require('../models/Task.model')
-
+const User = require("../models/User.model");
+const Task = require("../models/Task.model");
 
 const oauth2Client = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
   GOOGLE_REDIRECT_URL,
-  'http://localhost:3000'
+  "http://localhost:3000"
   //Este de cima, não estava no slack mas no da Rita
 );
 
 // generate a url that asks permissions for Blogger and Google Calendar scopes
 const scopes = [
-  'https://www.googleapis.com/auth/blogger',
-  'https://www.googleapis.com/auth/calendar',
+  "https://www.googleapis.com/auth/blogger",
+  "https://www.googleapis.com/auth/calendar",
 ];
 
 const url = oauth2Client.generateAuthUrl({
   // 'online' (default) or 'offline' (gets refresh_token)
-  access_type: 'offline',
+  access_type: "offline",
 
   // If you only need one scope you can pass it as a string
-  scope: scopes
+  scope: scopes,
 });
